b7992a6806572646f7de6547cecdf3419f98b35d
Jayakorn Karikan <jayakornk@gmail.com>
2021-08-01 15:00:17 +0700

    feat: change printWidth to large number
    prevent Prettier from changing the code style

--- packages/eslint-config-jka/prettier.config.js
+++ packages/eslint-config-jka/prettier.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  printWidth: 120,
+  printWidth: 999999,
   tabWidth: 2,
   useTabs: false,
   semi: true,
@@ -7,12 +7,6 @@ module.exports = {
   trailingComma: 'es5',
   bracketSpacing: true,
   overrides: [
-    {
-      files: 'package*.json',
-      options: {
-        printWidth: 1000,
-      },
-    },
     {
       files: '*.yml',
       options: {
